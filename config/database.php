a6c084424f6969388b900b674ffcff4a49ba9088
zigii24 <zigiarizona8@gmail.com>
2026-07-02 00:22:24 +0700

    mm

--- config/database.php
+++ config/database.php
@@ -1,23 +1,18 @@
 <?php
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$db   = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT') ?: 18862;
+// Hapus session_start() dari sini karena sudah dipanggil di file utama
+// session_start();
 
-// Tambahkan ini buat debugging kalau masih error
-if (empty($host)) {
-    die("Error: Variable MYSQLHOST tidak ditemukan di Railway. Cek tab Variables di Railway!");
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "agrocure_db";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
 }
 
-try {
-    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
-    $conn = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ]);
-} catch (PDOException $e) {
-    // Kalau error, dia bakal kasih tau host mana yang dicoba
-    die("Koneksi Gagal ke $host: " . $e->getMessage());
-}
+// Set charset to utf8
+mysqli_set_charset($conn, "utf8");
 ?>
--- info.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "PHP Version: " . phpversion();
-echo "<br>";
-if (extension_loaded('mysqli')) {
-    echo "mysqli is ENABLED";
-} else {
-    echo "mysqli is DISABLED";
-}
-?>
